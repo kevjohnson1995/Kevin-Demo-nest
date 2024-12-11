8e574e5e8d36b50565b45905eaa84283ea4eafa2
kevjohnson1995@yahoo.com <kevjohnson1995@yahoo.com>
2024-12-10 21:01:20 -0500

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2Njc3Mg/spectral.js?branch_slug=main&token=d512db03-21b6-4664-8d91-77378a84a6a6";
+export default { extends: ruleset };
